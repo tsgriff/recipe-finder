4ad73f9f02258f0084ce9eb586cb2b7f959106a2
Taylor Griffith <tgriffith14@hotmail.com>
2017-08-16 16:18:37 -0700

    login/logout toggle working

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './nav.css';
+import { connect } from 'react-redux';
+
+// USER INFO FOR LOGIN/LOGOUT TOGGLE //
+
+import { getUserInfo } from '../../ducks/user';
 
 class Nav extends Component {
+
+  componentDidMount() {
+    this.props.getUserInfo()
+    console.log(this.props.userInfo);
+  }
+
   render() {
+
+    const logIn = (
+      <a className="link-text" href="/auth"><li>Login</li></a>
+      )
+
+    const logOut = (
+      <a className="link-text" href="/auth/logout"><li>Logout</li></a>
+    )
+
     return (
       <section id="Nav">
         <div className="navbar-contain">
           <ul id="nav-links">
             <Link className="link-text" to="/"><li>Home</li></Link>
             <Link className="link-text" to="/about"><li>About</li></Link>
-            <Link className="link-text" to="/profile"><li>Profile</li></Link>
-            <a className="link-text" href="/auth"><li>Login</li></a>
+            {this.props.userInfo ? <Link className="link-text" to="/profile"><li>Profile</li></Link> : null }
+            {!this.props.userInfo ? logIn : logOut}
           </ul>
         </div>
       </section>
@@ -19,4 +39,10 @@ class Nav extends Component {
   }
 }
 
-export default Nav;
+function mapStateToProps(state) {
+    return {
+      userInfo: state.userReducer.userData
+    }
+  }
+
+export default connect(mapStateToProps, {getUserInfo})(Nav);
